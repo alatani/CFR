3fd552000205995659a525b707782ad76fdc2fc3
atsuhiro-narita <alayashiki@gmail.com>
2019-12-24 03:25:43 +0900

    --wip--

--- cfr.py
+++ cfr.py
@@ -9,7 +9,7 @@ import random
 import copy
 import bisect
 import itertools
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 
 Reward = int
@@ -23,7 +23,16 @@ class Environment:
     def information_set(self, agent_index: int) -> str:
         pass
 
-    def step(self, actions) -> (List[Reward], bool):
+    def step(self, index, action) -> bool:
+        pass
+
+    def is_terminal(self) -> bool:
+        pass
+
+    def get_reward(self, index) -> int:
+        pass
+
+    def turn_of(self) -> int:
         pass
 
 
@@ -49,15 +58,15 @@ class GJState():
         self.stars -= 1
 
     def alive(self):
-        if self.stars <= 0:
-            return False
-        for v in self.n_cards.values():
-            if v > 0:
-                return True
-        return False
+        return not self.is_terminal()
 
     def is_terminal(self):
-        return not self.alive()
+        if self.stars <= 0:
+            return True
+        for v in self.n_cards.values():
+            if v > 0:
+                return False
+        return True
 
     def get_usable_rsp(self) -> List[Action]:
         res = []
@@ -83,8 +92,14 @@ class GJ(Environment):
         super().__init__()
         self.agent_states = [copy.deepcopy(init_state) for _ in range(2)]
 
+        self.actions = [None, None]
+        self.__terminal = False
+        self.turn = 0
+
+    def turn_of(self):
+        return self.turn
+
     def information_set(self, agent_index: int):
-        res = []
         assert(agent_index == 0 or agent_index == 1)
 
         counterpart_index = 1 - agent_index
@@ -93,72 +108,76 @@ class GJ(Environment):
         counterpart_state = self.agent_states[counterpart_index]
         return (
             # 自分
-            my_state.n_cards['R'],
-            my_state.n_cards['S'],
-            my_state.n_cards['P'],
-            my_state.stars,
+            (my_state.n_cards['R'],
+             my_state.n_cards['S'],
+             my_state.n_cards['P'],
+             my_state.stars),
             # 相手
-            counterpart_state.n_cards['R'], #今まで何枚使ったかわかるはず
-            counterpart_state.n_cards['S'],
-            counterpart_state.n_cards['P'],
-            counterpart_state.stars,
+            (counterpart_state.n_cards['R'],  # 今まで何枚使ったかわかるはず
+             counterpart_state.n_cards['S'],
+             counterpart_state.n_cards['P'],
+             counterpart_state.stars),
         )
 
     def action_space(self, agent_index: int) -> List[Action]:
         state = self.agent_states[agent_index]
         return state.get_usable_rsp()
 
-    def backtrack(self, actions) -> (List[Reward], bool):
-        for states, action in zip(self.agent_states, actions):
-            states.restore_card(action)
-        # 勝利判定
-        hands = ["R", "S", "P"]
-        id1 = hands.index(actions[0])
-        id2 = hands.index(actions[1])
-        mod = (id2 - id1) % 3
-        r = 1
-        if mod == 1:
-            self.agent_states[0].lose()
-            self.agent_states[1].win()
-        elif mod == 2:
-            self.agent_states[0].win()
-            self.agent_states[1].lose()
-
-    def step(self, actions) -> (List[Reward], bool):
+    def step(self, index, action) -> bool:
         # 各agentのスコアを返す
-        assert(len(actions) == 2)
+        assert(index in [0, 1])
 
         # 使用したカードを減らす
-        for states, action in zip(self.agent_states, actions):
-            states.use_card(action)
+        # for states, action in zip(self.agent_states, actions):
+        #     states.use_card(action)
+        state = self.agent_states[index]
+        state.use_card(action)
+        self.actions[index] = action
 
-        # 勝利判定
-        hands = ["R", "S", "P"]
-        id1 = hands.index(actions[0])
-        id2 = hands.index(actions[1])
-        mod = (id2 - id1) % 3
-        r = 1
-        if mod == 1:
-            self.agent_states[0].win()
-            self.agent_states[1].lose()
-        elif mod == 2:
-            self.agent_states[0].lose()
-            self.agent_states[1].win()
+        if self.actions[0] and self.actions[1]:
+            # 勝利判定
+            hands = ["R", "S", "P"]
+            id1 = hands.index(self.actions[0])
+            id2 = hands.index(self.actions[1])
+            mod = (id2 - id1) % 3
+            r = 1
+            if mod == 1:
+                self.agent_states[0].win()
+                self.agent_states[1].lose()
+            elif mod == 2:
+                self.agent_states[0].lose()
+                self.agent_states[1].win()
 
-        rewards = self.calc_reward(actions)
-        # if rewards[0] == 0 and rewards[1] == 0:
-        if self.agent_states[0].alive() and self.agent_states[1].alive():
-            return rewards, False
+        # isterminal = self.is_terminal()
+        if self.actions[0] and self.actions[1]:
+            self.__terminal = (self.agent_states[0].is_terminal(
+            ) or self.agent_states[1].is_terminal())
         else:
-            return rewards, True
+            self.__terminal = False
 
-    def calc_reward(self, actions) -> List[Reward]:
-        if self.agent_states[0].stars > 0 and self.agent_states[1].stars == 0:
-            return [1, -1]
-        elif self.agent_states[0].stars == 0 and self.agent_states[1].stars > 0:
-            return [-1, 1]
-        else:
-            return [0, 0]
+        if index == 0:
+            assert(self.turn == 0)
+            self.turn = 1
+        elif index == 1:
+            assert(self.turn == 1)
+            self.turn = 0
+            self.actions = [None, None]
+        return self.__terminal
+
+    def is_terminal(self):
+        return self.__terminal
+
+    def get_reward(self, index) -> int:
+        if self.is_terminal():
+            counterpart = 1 - index
+            my_stars = self.agent_states[index].stars
+            your_stars = self.agent_states[counterpart].stars
+            if my_stars > your_stars:
+                return 1
+            if my_stars < your_stars:
+                return -1
+            return 0
+        return None
 
 
 @dataclass
@@ -189,9 +208,6 @@ class ManualAgent(Agent):
             print(f'{action} is NOT in action space.')
         return action
 
-    def update_policy(self, reward, agent_actions: List[Action]):
-        pass
-
 
 class RandomAgent(Agent):
     def __init__(self, index):
@@ -204,9 +220,6 @@ class RandomAgent(Agent):
         else:
             return None
 
-    def update_policy(self, reward, agent_actions: List[Action]):
-        pass
-
     def repr_state(self):
         return "random"
 
@@ -217,62 +230,6 @@ class FirstAgent(Agent):
 
     def act(self, reward, env: Environment, actions: List[Action]) -> Action:
         return env.action_space(self.index)[0]
-
-
-class RegretMatchingAgent(Agent):
-    old_env: Optional[Environment]
-
-    def __init__(self, index):
-        super().__init__(index, f"regret_matching({index})")
-        self.regrets = defaultdict(int)
-        self.total_reward = 0
-        self.old_env = None
-
-    def repr_state(self):
-        sum_of_regrets = max(sum(self.regrets.values() or [0]), 0.1)
-
-        res = {}
-        for k, v in self.regrets.items():
-            res[k] = v / sum_of_regrets
-        return res
-
-    def act(self, reward, env: Environment, actions: List[Action]) -> Action:
-        self.total_reward += reward
-
-        action_space = env.action_space(self.index)
-        # if not action_space:
-        #     return None
-        if self.old_env:
-            weights = [self.regrets[action]
-                       for action in action_space]
-            action = self.random_pick(
-                weights, env.action_space(self.index)
-            )
-
-        else:
-            action = random.choice(action_space)
-
-        self.old_env = copy.deepcopy(env)
-
-        return action
-
-    def update_policy(self, reward, agent_actions: List[Action]):
-        if self.old_env:
-            for action in self.old_env.action_space(self.index):
-                cf_actions = copy.copy(agent_actions)
-                cf_actions[self.index] = action
-
-                regret = max(0, self.old_env.calc_reward(
-                    cf_actions)[self.index] - reward)
-                self.regrets[action] += regret
-
-    def random_pick(self, weights, actions):
-        ac = list(itertools.accumulate(weights))
-        if ac[-1] > 0:
-            def f(): return bisect.bisect(ac, random.random()*ac[-1])
-            return actions[f()]
-        else:
-            return random.choice(actions)
 
 
 class CFR(Agent):
@@ -305,91 +262,74 @@ class CFR(Agent):
         else:
             return self.profile[(I, action)]
 
-    def __cfr(self, env: Environment, index, t, pi_me, pi_c, depth, chance=False, rewards=None):
-        # 自分のターン
-        if depth == 1:
-            print("First iteration:", env.agent_states, datetime.datetime.now())
-        if depth == 2:
-            print("    Second iteration:",
-                  env.agent_states, datetime.datetime.now())
+    def __cfr(self, env: Environment, index, t, pi0, pi1, depth):
+        if depth <= 2:
+            print(" " * depth + f"{depth}-th iteration:", env.agent_states,
+                f"actions:{env.actions} turn={env.turn_of()} terminal:{env.is_terminal()}  reward={env.get_reward(index)}(index={index})", datetime.datetime.now())
 
-        if rewards:
-            return rewards[index]
+        if env.is_terminal():
+            return env.get_reward(index)
 
         counterpart_index = 1 - index
-        action_space = env.action_space(index)
+        action_space = env.action_space(env.turn_of())
         vs = [0.0] * len(action_space)
         v = 0.0
-
         I = env.information_set(index)
 
-        if chance:
-            action = self.__sample_action(index, env)
-            prof = self.__get_profile(I, action, action_space)
-            return self.__cfr_counterpart(action, env, counterpart_index, t, pi_me, pi_c, depth)
-
-        search_node = random.randint(0, len(action_space)-1)
         for i, a in enumerate(action_space):
-            chance = i != search_node
-            chance = False
             prof = self.__get_profile(I, a, action_space)
-            vs[i] = self.__cfr_counterpart(
-                a, env, counterpart_index, t, prof * pi_me, pi_c, depth, chance)
+
+            new_env = copy.deepcopy(env)
+
+            if env.turn_of() == 0:
+                _ = new_env.step(0, a)
+                vs[i] = self.__cfr(new_env, index, t,
+                                   prof * pi0, pi1, depth+1)
+            elif env.turn_of() == 1:
+                # print(new_env.agent_states, a)
+                _ = new_env.step(1, a)
+                vs[i] = self.__cfr(new_env, index, t, pi0,
+                                   prof * pi1, depth+1)
+            else:
+                raise ValueError()
 
             v += prof*vs[i]
+        if I == ((0, 1, 1, 1), (0, 1, 1, 1)) and index == 0:
+            print("depth", depth)
+            print("index", index)
+            print("env", env.agent_states)
+            print("action_space", action_space)
+            print("vs", vs)
+            print("v", v)
+            print("turn", env.turn_of())
 
-        for i, a in enumerate(action_space):
-            prof = self.__get_profile(I, a, action_space)
-            self.cum_regrets[(I, a)] += pi_c * (vs[i] - v)
-            self.cum_strategy[(I, a)] += pi_me * prof
-        self.__update_profiles(I, action_space)
-        return v
-
-    def __cfr_counterpart(self, my_action, env: Environment, counterpart_index, t, pi_me, pi_c, depth, chance=False):
-        # 相手のターン
-        index = 1 - counterpart_index
-        action_space = env.action_space(counterpart_index)
-        I = env.information_set(counterpart_index)
-        actions = [None, None]
-        actions[index] = my_action
-
-        vs = [0.0] * len(action_space)
-        v = 0.0
-        search_node = random.randint(0, len(action_space)-1)
-        for i, a in enumerate(action_space):
-            #chance = False
-            actions[counterpart_index] = a
-
-            # new_env = env
-            new_env = copy.deepcopy(env)
-            rewards, done = new_env.step(actions)
-            if not done:
-                rewards = None
-
-            Inext = new_env.information_set(counterpart_index)
-
-            prof = self.__get_profile(I, a, action_space)
-            vs[i] = self.__cfr(new_env, index, t, pi_me,
-                               prof*pi_c, depth+1, chance, rewards)
-            v += prof * vs[i]
-            # new_env.backtrack(actions)
+        my_pi = [pi0, pi1][index]
+        your_pi = [pi0, pi1][counterpart_index]
+        if env.turn_of() == index:
+            for i, a in enumerate(action_space):
+                prof = self.__get_profile(I, a, action_space)
+                self.cum_regrets[(I, a)] += your_pi * (vs[i] - v)
+                self.cum_strategy[(I, a)] += my_pi * prof
+            self.__update_profiles(I, action_space)
         return v
 
     def __update_profiles(self, I, action_space):
         denominator = 0.0
         for a in action_space:
-            denominator += max(self.cum_regrets[(I, a)], 0)
+            denominator += max(self.cum_regrets[(I, a)], 0.0)
         if denominator > 0.0001:
+
             for a in action_space:
-                self.profile[(I, a)] = max(self.cum_regrets[(I, a)],0) / denominator
+                prof = max(self.cum_regrets[(I, a)], 0.0) / denominator
+                self.profile[(I, a)] = prof
         else:
             d = 1.0 / len(action_space)
             for a in action_space:
                 self.profile[(I, a)] = d
 
-    def __sample_action(self, agent_index, h: Environment, debug=False) -> Action:
-        I = h.information_set(agent_index)
-        action_space = h.action_space(agent_index)
+    def __sample_action(self, agent_index, env: Environment, debug=False) -> Action:
+        I = env.information_set(agent_index)
+        action_space = env.action_space(agent_index)
         profiles = []
         for a in action_space:
             if (I, a) in self.profile:
@@ -402,20 +342,16 @@ class CFR(Agent):
             profiles.append(prof)
         picked = self.random_pick(profiles, action_space)
 
-        if debug:
-            print(
-                f'picked {picked} from {action_space}.  prof={profiles}. key={(I, picked)}')
+        print(
+            f'picked {picked} from {action_space}.  prof={profiles}. key={(I, picked)}')
         return picked
 
     def repr_state(self):
         pass
 
     def act(self, reward, env: Environment, actions: List[Action]) -> Action:
-        return self.__sample_action(self.index, env, True)
+        return self.__sample_action(self.index, env)
         # return action
-
-    def update_policy(self, reward, agent_actions: List[Action]):
-        pass
 
     def random_pick(self, weights, actions):
         ac = list(itertools.accumulate(weights))
@@ -428,6 +364,7 @@ class CFR(Agent):
 
 def gameplay(env_gen, agents: List[Agent], games: int):
     total_reward = [0]*len(agents)
+    result = Counter()
 
     for t in range(games):
         env = env_gen()
@@ -437,20 +374,23 @@ def gameplay(env_gen, agents: List[Agent], games: int):
             actions = [agent.act(reward, env, actions)
                        for agent, reward in zip(agents, rewards)]
 
-            rewards, done = env.step(actions)
-
-            for reward, agent in zip(rewards, agents):
-                agent.update_policy(reward, actions)
+            for i, action in enumerate(actions):
+                done = env.step(i, action)
+                if done:
+                    break
 
             for i in range(len(agents)):
-                total_reward[i] += rewards[i]
+                total_reward[i] += env.get_reward(i) or 0
+                rewards[i] += env.get_reward(i) or 0
             if done:
                 break
 
+        result.update([rewards[1]])
         match_result = ', '.join(
             [f'{agent.name}: {tr}' for agent, tr in zip(agents, total_reward)]
         )
         print(match_result)
+        print(result)
 
     # match_result = ', '.join(
     #     [f'{agent.name}: {action} -> {tr}' for action,
@@ -465,9 +405,20 @@ def init_agents(clss):
 
 
 if __name__ == "__main__2":
-    with open("gj2222.pickle", "rb") as f:
+    # Ir = (((0, 1, 1, 1), (0, 1, 1, 1)), 'R')
+    Is = (((0, 1, 1, 1), (0, 1, 1, 1)), 'S')
+    Ip = (((0, 1, 1, 1), (0, 1, 1, 1)), 'P')
+    Is = [Is, Ip]
+    with open("gj1111.pickle", "rb") as f:
         cfr = pickle.load(f)
-        print(cfr.profile)
+        for I in Is:
+            print(cfr.profile[I])
+        print("---")
+        for I in Is:
+            print(cfr.cum_regrets[I])
+        print("---")
+        for I in Is:
+            print(cfr.cum_strategy[I])
     sys.exit()
 
 if __name__ == "__main__":
@@ -475,10 +426,16 @@ if __name__ == "__main__":
     print("now=", datetime.datetime.now())
     # env = RSP(["R", "S", "P"], rounds=10000)
 
-    env = GJ(GJState({'R': 2, 'S': 2, 'P': 2}, stars=2))
-    cfr = CFR(1).train(env, T=10)
-    with open("gj2222.pickle", "wb") as f:
-        pickle.dump(cfr, f)
+    # env = GJ(GJState({'R': 1, 'S': 1, 'P': 1}, stars=1))
+    # cfr = CFR(1).train(env, T=3)
+    # with open("gj1111.pickle", "wb") as f:
+    #     pickle.dump(cfr, f)
+    #sys.exit()
+
+    # env = GJ(GJState({'R': 2, 'S': 2, 'P': 2}, stars=2))
+    # cfr = CFR(1).train(env, T=10)
+    # with open("gj2222.pickle", "wb") as f:
+    #     pickle.dump(cfr, f)
 
     # env = GJ(GJState({'R': 4, 'S': 4, 'P': 4}, stars=4))
     # with open("gj4444.pickle", "wb") as f:
@@ -495,10 +452,10 @@ if __name__ == "__main__":
     # with open("gj3333.pickle", "wb") as f:
     #     pickle.dump(cfr, f)
 
-    # env = GJ(GJState({'R': 2, 'S': 2, 'P': 2}, stars=2))
-    # cfr = CFR(1).train(env, T=1)
-    # with open("gj2222.pickle", "wb") as f:
-    #     pickle.dump(cfr, f)
+    env = GJ(GJState({'R': 2, 'S': 2, 'P': 2}, stars=2))
+    cfr = CFR(1).train(env, T=10)
+    with open("gj2222.pickle", "wb") as f:
+        pickle.dump(cfr, f)
 
     # env = GJ(GJState({'R': 4, 'S': 4, 'P': 4}, stars=3))
     # cfr = CFR(1).train(env, T=1)
@@ -507,19 +464,22 @@ if __name__ == "__main__":
     # sys.exit()
 
     # with open("gj3333.pickle", "rb") as f:
-    #     cfr = pickle.load(f)
-    with open("gj2222.pickle", "rb") as f:
-        cfr = pickle.load(f)
-    print(cfr)
 
-    print("============== FINISHED TRAINING ==============")
-    cfr.index = 1
-    # agents = [ManualAgent(0), cfr]
-    agents = [RandomAgent(0), cfr]
-    #agents = [RandomAgent(0), RandomAgent(1)]
-    # agents = [RandomAgent(0), RandomAgent(1)]
-    # env = GJ(GJState.single())
+
+    #     cfr = pickle.load(f)
+    # with open("gj1111.pickle", "rb") as f:
+    #     cfr = pickle.load(f)
+    # cfr.index = 1
+    # agents = [RandomAgent(0), cfr]
+    # def env_gen(): return GJ(GJState({'R': 1, 'S': 1, 'P': 1}, stars=1))
+    # gameplay(env_gen, agents, games=10000)
+
+    # with open("gj2222.pickle", "rb") as f:
+    #     cfr = pickle.load(f)
+    # cfr.index = 1
+    # agents = [RandomAgent(0), cfr]
+    # def env_gen(): return GJ(GJState({'R': 2, 'S': 2, 'P': 2}, stars=2))
+    # gameplay(env_gen, agents, games=10000)
 
     # def env_gen(): return GJ(GJState({'R': 3, 'S': 3, 'P': 3}, stars=3))
-    def env_gen(): return GJ(GJState({'R': 2, 'S': 2, 'P': 2}, stars=2))
-    gameplay(env_gen, agents, games=10000)
+    #def env_gen(): return GJ(GJState({'R': 1, 'S': 1, 'P': 1}, stars=1))
